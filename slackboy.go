0a26c732a4f952bc8e213d5f534e819fe70416bd
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2017-07-25 16:26:31 +0700

    first commit

---
+++ slackboy.go
@@ -0,0 +1,159 @@
+package slackboy
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var httpClient = http.Client{
+	Timeout: 5 * time.Second,
+}
+
+const (
+	successType = iota
+	infoType
+	warningType
+	errorType
+)
+
+type Message struct {
+	Channel         string
+	Text            string
+	AttachmentColor string
+	AttachmentText  string
+}
+
+type messageMap map[int]*Message
+
+type SlackboyTags map[string]string
+
+type Options struct {
+	Env  string
+	Tags SlackboyTags
+
+	WebhookURL     string
+	DefaultChannel string
+	SuccessChannel string
+	InfoChannel    string
+	WarningChannel string
+	ErrorChannel   string
+}
+
+type SlackBoy struct {
+	message messageMap
+	opt     Options
+}
+
+func New(o Options) *SlackBoy {
+	msgMap := messageMap{}
+
+	msgMap[successType] = &Message{Channel: o.SuccessChannel, AttachmentColor: "good"}
+	msgMap[infoType] = &Message{Channel: o.InfoChannel, AttachmentColor: "#3AA3E3"}
+	msgMap[warningType] = &Message{Channel: o.WarningChannel, AttachmentColor: "warning"}
+	msgMap[errorType] = &Message{Channel: o.ErrorChannel, AttachmentColor: "danger"}
+
+	return &SlackBoy{message: msgMap, opt: o}
+}
+
+func (s *SlackBoy) getMessage(msgType int) *Message {
+	if msg, ok := s.message[msgType]; ok {
+		return msg
+	}
+
+	return &Message{}
+}
+
+func (s *SlackBoy) Success(text, snip string) {
+	msg := s.getMessage(successType)
+	msg.Text = text
+	msg.AttachmentText = snip
+
+	s.Post(msg)
+}
+
+func (s *SlackBoy) Info(text, snip string) {
+	msg := s.getMessage(infoType)
+	msg.Text = text
+	msg.AttachmentText = snip
+
+	s.Post(msg)
+}
+
+func (s *SlackBoy) Warning(text, snip string) {
+	msg := s.getMessage(warningType)
+	msg.Text = text
+	msg.AttachmentText = snip
+
+	s.Post(msg)
+}
+
+func (s *SlackBoy) Error(text, snip string) {
+	msg := s.getMessage(errorType)
+	msg.Text = text
+	msg.AttachmentText = snip
+
+	s.Post(msg)
+}
+
+func (s *SlackBoy) Post(msg *Message) {
+	channel := msg.Channel
+	if channel == "" {
+		channel = s.opt.DefaultChannel
+	}
+
+	tags := []string{fmt.Sprintf("`env: %s`", s.opt.Env)}
+	if len(s.opt.Tags) > 0 {
+		for k, v := range s.opt.Tags {
+			tags = append(tags, fmt.Sprintf("`%s: %s`", k, v))
+		}
+	}
+	tagString := strings.Join(tags, " ")
+
+	payload := map[string]interface{}{
+		"channel":    channel,
+		"link_names": 1,
+		"attachments": []map[string]interface{}{
+			map[string]interface{}{
+				"title":     msg.Text,
+				"color":     msg.AttachmentColor,
+				"text":      msg.AttachmentText + "\n" + tagString,
+				"mrkdwn_in": []string{"text"},
+			},
+		},
+	}
+
+	s.post(payload)
+}
+
+func (s *SlackBoy) post(payload map[string]interface{}) {
+	body, err := json.Marshal(payload)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	resp, err := httpClient.Post(s.opt.WebhookURL, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(data))
+	}
+
+	return
+}
---
+++ slackboy_test.go
@@ -0,0 +1,110 @@
+package slackboy
+
+import "testing"
+
+func TestPostToDefault(t *testing.T) {
+	opt := Options{
+		Env:        "production",
+		WebhookURL: "https://hooks.slack.com/services/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",
+		Tags:       SlackboyTags{"host": "127.0.0.1"},
+	}
+	slackBoy := New(opt)
+
+	slackBoy.Success("Success 1", "Success description 1")
+	slackBoy.Info("Info 1", "Info description 1")
+}
+
+func TestPostToDefault2(t *testing.T) {
+	opt := Options{
+		Env:        "production",
+		WebhookURL: "https://hooks.slack.com/services/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",
+		Tags:       SlackboyTags{"host": "127.0.0.1"},
+	}
+	slackBoy := New(opt)
+
+	slackBoy.Success("Success 1", "Success description 1")
+	slackBoy.Info("Info 1", "Info description 1")
+
+	opt2 := Options{
+		Env:        "production",
+		WebhookURL: "https://hooks.slack.com/services/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",
+		Tags:       SlackboyTags{"host": "127.0.0.1", "user": "@ariefrahmansyah"},
+	}
+	slackBoy2 := New(opt2)
+
+	slackBoy2.Warning("Warning 1", "Warning description 1")
+	slackBoy2.Error("Error 1", "Error description 1")
+}
+
+func TestPostToSuccess(t *testing.T) {
+	opt := Options{
+		Env:            "production",
+		WebhookURL:     "https://hooks.slack.com/services/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",
+		SuccessChannel: "success",
+		InfoChannel:    "info",
+		WarningChannel: "warning",
+		ErrorChannel:   "error",
+		Tags:           SlackboyTags{"host": "127.0.0.1"},
+	}
+	slackBoy := New(opt)
+
+	slackBoy.Success("Success 1", "Success description 1")
+}
+
+func TestPostToInfo(t *testing.T) {
+	opt := Options{
+		Env:            "production",
+		WebhookURL:     "https://hooks.slack.com/services/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",
+		SuccessChannel: "success",
+		InfoChannel:    "info",
+		WarningChannel: "warning",
+		ErrorChannel:   "error",
+		Tags:           SlackboyTags{"host": "127.0.0.1"},
+	}
+	slackBoy := New(opt)
+
+	slackBoy.Info("Info 1", "Info description 1")
+}
+
+func TestPostToInfoWithLink(t *testing.T) {
+	opt := Options{
+		Env:            "production",
+		WebhookURL:     "https://hooks.slack.com/services/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",
+		SuccessChannel: "success",
+		InfoChannel:    "info",
+		WarningChannel: "warning",
+		ErrorChannel:   "error",
+		Tags:           SlackboyTags{"host": "127.0.0.1"},
+	}
+	slackBoy := New(opt)
+
+	slackBoy.Info("Info 1", "Link to Google: https://www.google.com")
+}
+
+func TestPostToWarning(t *testing.T) {
+	opt := Options{
+		Env:            "production",
+		WebhookURL:     "https://hooks.slack.com/services/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",
+		SuccessChannel: "success",
+		InfoChannel:    "info",
+		WarningChannel: "warning",
+		ErrorChannel:   "error",
+	}
+	slackBoy := New(opt)
+
+	slackBoy.Warning("Warning 1", "Warning description 1")
+}
+
+func TestPostToError(t *testing.T) {
+	opt := Options{
+		Env:            "production",
+		WebhookURL:     "https://hooks.slack.com/services/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",
+		SuccessChannel: "success",
+		InfoChannel:    "info",
+		WarningChannel: "warning",
+		ErrorChannel:   "error",
+	}
+	slackBoy := New(opt)
+
+	slackBoy.Error("Error 1", "Error description 1")
+}
